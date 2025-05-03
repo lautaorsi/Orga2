cf92cb9f721fbd08c30d0b143a5f570766f5ef3d
lauta <lautaorsi@gmail.com>
2025-05-03 01:08:31 +0000

    ex1.a 2023C1R finished

--- primeros_parciales/2023C1R/solucion/ej1/ej1.asm
+++ primeros_parciales/2023C1R/solucion/ej1/ej1.asm
@@ -2,14 +2,130 @@ global acumuladoPorCliente_asm
 global en_blacklist_asm
 global blacklistComercios_asm
 
+extern malloc
+extern strcmp
+
 ;########### SECCION DE TEXTO (PROGRAMA)
 section .text
 
-
+;RDI -> uint8_t cantidad de pagos
+;RSI -> pago_t * pagos
+;rax -> uint32_t* total de pagos 
 acumuladoPorCliente_asm:
+	;prologo
+	push rbp
+	mov rbp, rsp
+	push rbx
+	push r15
+	push r14
+	push r13
+
+
+	push rdi
+	push RSI ;preservo registros volatiles
+
+	and rdi, 0 
+	mov rdi, 80
+	call malloc ;armo array de enteros (10 pointers = 80 bytes)
+
+	mov rdi, rax
+	
+	pop r14 ;r13 = cantidad de pagos
+	pop r13 ; r14 = arreglo de pagos
+	mov r15, rax ; r15 = puntero array total
+
+	;setteo todos los valores del malloc a cero
+	and qword [r15], 0
+	and qword [r15 + 8], 0
+	and qword [r15 + 16], 0
+	and qword [r15 + 24], 0
+	and qword [r15 + 32], 0
+	and qword [r15 + 40], 0
+
+
+
+	cmp r13, 0
+	je .fin ;si no hay pagos no itero
+
+	.loop:
+	movzx r8, byte [r14 + 16] ;obtengo id de cliente
+	movzx r10, byte [r14] ;guardo en r10 el valor de pago
+	movzx rbx, byte [r14 + 17] ;guardo aprobado
+	cmp rbx, 0 ;si no esta aprobado paso al sig
+	je .siguiente
+	mov r9, r15 ;indice para iterar
+	cmp r8, 0 ;si es id 0 lo guardo en el lugar
+	jne .buscar_indice
+	add [r15], r10
+	jmp .siguiente
+		.buscar_indice:
+			dec r8
+			add r9, 4
+			cmp r8, 0
+			je .agregar_en_indice
+			jmp .buscar_indice
+			.agregar_en_indice:
+				add [r9], r10
+	.siguiente:
+	dec r13
+	cmp r13, 0
+	je .fin 
+	add r14, 24 ;paso al siguiente pago
+	jmp .loop
+
+	.fin:
+	mov rax, r15
+	pop r13
+	pop r14
+	pop r15
+	pop rbx
+	pop rbp
 	ret
 
+
+;rdi -> char* comercio
+;rsi -> char** lista de strings
+;rdx -> longitud de lista
 en_blacklist_asm:
+	;prologo
+	push rbp
+	mov rbp, rsp
+	push rbx
+	push r15
+	push r14
+	push r13
+
+
+	and rax, 0
+
+	cmp rdx, 0
+	je .fin
+
+	mov rbx, rdi ;rbx = nombre
+	mov r15, rdx ;r15 = cant de nombres
+	mov r13, rsi ;r13 = lista de nombres
+	.loop:
+		mov rdi, rbx
+		mov rsi, [r13]
+		call strcmp
+		cmp rax, 0
+		jne .no_sumar
+		inc rax
+		jmp .fin
+		.no_sumar:
+		dec r15
+		cmp r15, 0
+		je .fin
+		add r13, 8
+		jmp .loop
+
+		
+	.fin:
+	pop r13
+	pop r14
+	pop r15
+	pop rbx
+	pop rbp
 	ret
 
 blacklistComercios_asm:
--- primeros_parciales/2023C1R/solucion/ej1/main.c
+++ primeros_parciales/2023C1R/solucion/ej1/main.c
@@ -7,6 +7,48 @@
 #include "ej1.h"
 
 int main (void){
+
+    pago_t* unArr = malloc(20 * sizeof(pago_t));  // We allocate memory for 20 elements
+
+    // Populate the array with pago_t values
+
+    unArr[0].aprobado = 1; unArr[0].cliente = 0; unArr[0].comercio = "comercio0"; unArr[0].monto = 200;
+    unArr[1].aprobado = 0; unArr[1].cliente = 0; unArr[1].comercio = "comercio0"; unArr[1].monto = 100;
+
+    unArr[2].aprobado = 1; unArr[2].cliente = 1; unArr[2].comercio = "comercio1"; unArr[2].monto = 200;
+    unArr[3].aprobado = 0; unArr[3].cliente = 1; unArr[3].comercio = "comercio1"; unArr[3].monto = 100;
+
+    unArr[4].aprobado = 1; unArr[4].cliente = 2; unArr[4].comercio = "comercio2"; unArr[4].monto = 200;
+    unArr[5].aprobado = 0; unArr[5].cliente = 2; unArr[5].comercio = "comercio2"; unArr[5].monto = 100;
+
+    unArr[6].aprobado = 1; unArr[6].cliente = 3; unArr[6].comercio = "comercio3"; unArr[6].monto = 200;
+    unArr[7].aprobado = 0; unArr[7].cliente = 3; unArr[7].comercio = "comercio3"; unArr[7].monto = 100;
+
+    unArr[8].aprobado = 1; unArr[8].cliente = 4; unArr[8].comercio = "comercio4"; unArr[8].monto = 200;
+    unArr[9].aprobado = 0; unArr[9].cliente = 4; unArr[9].comercio = "comercio4"; unArr[9].monto = 100;
+
+    unArr[10].aprobado = 1; unArr[10].cliente = 5; unArr[10].comercio = "comercio5"; unArr[10].monto = 200;
+    unArr[11].aprobado = 0; unArr[11].cliente = 5; unArr[11].comercio = "comercio5"; unArr[11].monto = 100;
+
+    unArr[12].aprobado = 1; unArr[12].cliente = 6; unArr[12].comercio = "comercio6"; unArr[12].monto = 200;
+    unArr[13].aprobado = 0; unArr[13].cliente = 6; unArr[13].comercio = "comercio6"; unArr[13].monto = 100;
+
+    unArr[14].aprobado = 1; unArr[14].cliente = 7; unArr[14].comercio = "comercio7"; unArr[14].monto = 200;
+    unArr[15].aprobado = 0; unArr[15].cliente = 7; unArr[15].comercio = "comercio7"; unArr[15].monto = 100;
+
+    unArr[16].aprobado = 1; unArr[16].cliente = 8; unArr[16].comercio = "comercio8"; unArr[16].monto = 200;
+    unArr[17].aprobado = 0; unArr[17].cliente = 8; unArr[17].comercio = "comercio8"; unArr[17].monto = 100;
+
+    unArr[18].aprobado = 1; unArr[18].cliente = 9; unArr[18].comercio = "comercio9"; unArr[18].monto = 200;
+    unArr[19].aprobado = 0; unArr[19].cliente = 9; unArr[19].comercio = "comercio9"; unArr[19].monto = 100;
+
+    // Now you can pass unArr to the function
+    uint32_t* otroArr = acumuladoPorCliente_asm(20, unArr);
+
+
+    
+    // Don't forget to free the allocated memory after you're done
+    free(unArr);
  
 }
 
